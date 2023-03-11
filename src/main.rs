52975d1ba6f1f121159f4d91334fe6f5961d544d
Ian Bygrave <ian@bygrave.me.uk>
2023-03-11 21:45:46 +0000

    serial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
     let mut led = pins.d13.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let mut count = 0u16;
 
+    ufmt::uwriteln!(&mut serial, "Yo Monde!").void_unwrap();
     loop {
         led.toggle();
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(count);
+        count += 1;
+        if count == 100 {
+            ufmt::uwriteln!(&mut serial, "Toggle").void_unwrap();
+            count = 0;
+        }
     }
 }
