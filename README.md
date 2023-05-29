2f20adf3c4352ad9d3561d0b1b8f7076d0b1423e
Ian Bygrave <ian@bygrave.me.uk>
2023-05-29 20:29:37 +0100

    gdb connection to qemu

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Blink an LED on Arduino UNO
 ===========================
 
-Just a copy of the avr-hal-template.
+From a copy of the avr-hal-template.
+
+`cargo run` tries to load the target on an attached arduino.
+If that fails it runs the target with `qemu-system-avr`
+and connects `avr-gdb` to it.
 
 ## License
 
---
+++ scripts/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ex
+
+exe=$1
+
+ravedude uno -cb 57600 $exe && exit
+
+echo "Falling back to QEMU"
+
+QEMU_CMD="qemu-system-avr -machine uno -bios $exe -nographic"
+
+# gdb
+QEMU_CMD="$QEMU_CMD -s -S"
+fg_cmd () {
+    cgdb -d avr-gdb -- -ex 'target remote :1234' $exe
+}
+
+# serial
+#QEMU_CMD="$QEMU_CMD -serial tcp::5678,server=on -nographic"
+#fg_cmd () {
+#    sleep 1
+#    telnet localhost 5678
+#}
+
+finish () {
+    kill -9 $QEMU_PID
+}
+
+$QEMU_CMD &
+QEMU_PID=$!
+trap finish EXIT
+fg_cmd
+
