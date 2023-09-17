a684203c7ecd3fd1034996bcca3c294fda57b192
Ian Bygrave <ian@bygrave.me.uk>
2023-09-17 17:43:43 +0100

    Morse

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,90 @@
 #![no_main]
 
 use arduino_hal::prelude::*;
+use embedded_hal::digital::v2::OutputPin;
 use panic_halt as _;
+
+const WPM: u16 = 10;
+const T_DIT_MS: u16 = (60000) / (50 * WPM);
+const T_DAH_MS: u16 = 3 * T_DIT_MS;
+const T_ICS_MS: u16 = 3 * T_DIT_MS;
+const T_IWS_MS: u16 = 7 * T_DIT_MS;
+
+fn morse_char<P, E>(led: &mut P, c: char) -> Result<(), E>
+where
+    P: OutputPin<Error = E>,
+{
+    match c {
+        'g' => {
+            // - - .
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_low()?;
+        }
+        'm' => {
+            // - -
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+        }
+        'o' => {
+            // - - -
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DAH_MS);
+            led.set_low()?;
+        }
+
+        's' => {
+            // . . .
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_low()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_high()?;
+            arduino_hal::delay_ms(T_DIT_MS);
+            led.set_low()?;
+        }
+        _ => {
+            arduino_hal::delay_ms(T_IWS_MS - T_ICS_MS);
+        }
+    }
+    arduino_hal::delay_ms(T_ICS_MS);
+    Ok(())
+}
+
+fn morse_string<P, E>(led: &mut P, text: &str) -> Result<(), E>
+where
+    P: OutputPin<Error = E>,
+{
+    for char in text.chars() {
+        morse_char(led, char.to_ascii_lowercase())?;
+    }
+    Ok(())
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -10,16 +93,11 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
     let mut led = pins.d13.into_output();
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
-    let mut count = 0u16;
 
-    ufmt::uwriteln!(&mut serial, "Yo Monde!").void_unwrap();
+    ufmt::uwriteln!(&mut serial, "Yo Monde!\n").void_unwrap();
+    led.set_low();
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(count);
-        count += 1;
-        if count == 100 {
-            ufmt::uwriteln!(&mut serial, "Toggle").void_unwrap();
-            count = 0;
-        }
+        morse_string(&mut led, "SOS omg").unwrap();
+        arduino_hal::delay_ms(3000);
     }
 }
